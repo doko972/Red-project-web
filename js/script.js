3fb0c1768528ac77272b7227aa64d7515f809f2e
doko972 david <doko972@gmail.com>
2024-05-31 15:03:14 +0200

    slider card hidden fixed

--- js/script.js
+++ js/script.js
@@ -2,53 +2,46 @@ const repeat = false;
 const noArrows = false;
 const noBullets = false;
 
-
 const container = document.querySelector('.slider-container');
-var slide = document.querySelectorAll('.slider-single');
-var slideTotal = slide.length - 1;
-var slideCurrent = -1;
+const slide = document.querySelectorAll('.slider-single');
+const slideTotal = slide.length - 1;
+let slideCurrent = -1;
 
 function initBullets() {
-    if (noBullets) {
-        return;
-    }
+    if (noBullets) return;
+
     const bulletContainer = document.createElement('div');
-    bulletContainer.classList.add('bullet-container')
+    bulletContainer.classList.add('bullet-container');
+    
     slide.forEach((elem, i) => {
         const bullet = document.createElement('div');
-        bullet.classList.add('bullet')
-        bullet.id = `bullet-index-${i}`
-        bullet.addEventListener('click', () => {
-            goToIndexSlide(i);
-        })
+        bullet.classList.add('bullet');
+        bullet.id = `bullet-index-${i}`;
+        bullet.addEventListener('click', () => goToIndexSlide(i));
         bulletContainer.appendChild(bullet);
         elem.classList.add('proactivede');
-    })
+    });
+    
     container.appendChild(bulletContainer);
 }
 
 function initArrows() {
-    if (noArrows) {
-        return;
-    }
-    const leftArrow = document.createElement('a')
+    if (noArrows) return;
+
+    const leftArrow = document.createElement('a');
     const iLeft = document.createElement('i');
-    iLeft.classList.add('fa')
-    iLeft.classList.add('fa-arrow-left')
-    leftArrow.classList.add('slider-left')
-    leftArrow.appendChild(iLeft)
-    leftArrow.addEventListener('click', () => {
-        slideLeft();
-    })
-    const rightArrow = document.createElement('a')
+    iLeft.classList.add('fa', 'fa-arrow-left');
+    leftArrow.classList.add('slider-left');
+    leftArrow.appendChild(iLeft);
+    leftArrow.addEventListener('click', slideLeft);
+
+    const rightArrow = document.createElement('a');
     const iRight = document.createElement('i');
-    iRight.classList.add('fa')
-    iRight.classList.add('fa-arrow-right')
-    rightArrow.classList.add('slider-right')
-    rightArrow.appendChild(iRight)
-    rightArrow.addEventListener('click', () => {
-        slideRight();
-    })
+    iRight.classList.add('fa', 'fa-arrow-right');
+    rightArrow.classList.add('slider-right');
+    rightArrow.appendChild(iRight);
+    rightArrow.addEventListener('click', slideRight);
+
     container.appendChild(leftArrow);
     container.appendChild(rightArrow);
 }
@@ -56,9 +49,7 @@ function initArrows() {
 function slideInitial() {
     initBullets();
     initArrows();
-    setTimeout(function () {
-        slideRight();
-    }, 500);
+    setTimeout(slideRight, 500);
 }
 
 function updateBullet() {
@@ -68,165 +59,87 @@ function updateBullet() {
             if (i === slideCurrent) {
                 elem.classList.add('active');
             }
-        })
+        });
     }
     checkRepeat();
 }
 
 function checkRepeat() {
     if (!repeat) {
+        const leftArrow = document.querySelector('.slider-left');
+        const rightArrow = document.querySelector('.slider-right');
+
         if (slideCurrent === slide.length - 1) {
             slide[0].classList.add('not-visible');
             slide[slide.length - 1].classList.remove('not-visible');
             if (!noArrows) {
-                document.querySelector('.slider-right').classList.add('not-visible')
-                document.querySelector('.slider-left').classList.remove('not-visible')
+                rightArrow.classList.add('not-visible');
+                leftArrow.classList.remove('not-visible');
             }
-        }
-        else if (slideCurrent === 0) {
+        } else if (slideCurrent === 0) {
             slide[slide.length - 1].classList.add('not-visible');
             slide[0].classList.remove('not-visible');
             if (!noArrows) {
-                document.querySelector('.slider-left').classList.add('not-visible')
-                document.querySelector('.slider-right').classList.remove('not-visible')
+                leftArrow.classList.add('not-visible');
+                rightArrow.classList.remove('not-visible');
             }
         } else {
             slide[slide.length - 1].classList.remove('not-visible');
             slide[0].classList.remove('not-visible');
             if (!noArrows) {
-                document.querySelector('.slider-left').classList.remove('not-visible')
-                document.querySelector('.slider-right').classList.remove('not-visible')
+                leftArrow.classList.remove('not-visible');
+                rightArrow.classList.remove('not-visible');
             }
         }
     }
 }
 
 function slideRight() {
-    if (slideCurrent < slideTotal) {
-        slideCurrent++;
-    } else {
-        slideCurrent = 0;
-    }
+    slideCurrent = (slideCurrent < slideTotal) ? slideCurrent + 1 : 0;
 
-    if (slideCurrent > 0) {
-        var preactiveSlide = slide[slideCurrent - 1];
-    } else {
-        var preactiveSlide = slide[slideTotal];
-    }
-    var activeSlide = slide[slideCurrent];
-    if (slideCurrent < slideTotal) {
-        var proactiveSlide = slide[slideCurrent + 1];
-    } else {
-        var proactiveSlide = slide[0];
-
-    }
+    const preactiveSlide = (slideCurrent > 0) ? slide[slideCurrent - 1] : slide[slideTotal];
+    const activeSlide = slide[slideCurrent];
+    const proactiveSlide = (slideCurrent < slideTotal) ? slide[slideCurrent + 1] : slide[0];
 
     slide.forEach((elem) => {
-        var thisSlide = elem;
-        if (thisSlide.classList.contains('preactivede')) {
-            thisSlide.classList.remove('preactivede');
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.add('proactivede');
-        }
-        if (thisSlide.classList.contains('preactive')) {
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.remove('proactivede');
-            thisSlide.classList.add('preactivede');
-        }
+        elem.classList.remove('preactivede', 'preactive', 'active', 'proactive');
+        elem.classList.add('proactivede');
     });
-    preactiveSlide.classList.remove('preactivede');
-    preactiveSlide.classList.remove('active');
-    preactiveSlide.classList.remove('proactive');
-    preactiveSlide.classList.remove('proactivede');
+
     preactiveSlide.classList.add('preactive');
-
-    activeSlide.classList.remove('preactivede');
-    activeSlide.classList.remove('preactive');
-    activeSlide.classList.remove('proactive');
-    activeSlide.classList.remove('proactivede');
     activeSlide.classList.add('active');
-
-    proactiveSlide.classList.remove('preactivede');
-    proactiveSlide.classList.remove('preactive');
-    proactiveSlide.classList.remove('active');
-    proactiveSlide.classList.remove('proactivede');
     proactiveSlide.classList.add('proactive');
 
     updateBullet();
 }
 
 function slideLeft() {
-    if (slideCurrent > 0) {
-        slideCurrent--;
-    } else {
-        slideCurrent = slideTotal;
-    }
+    slideCurrent = (slideCurrent > 0) ? slideCurrent - 1 : slideTotal;
 
-    if (slideCurrent < slideTotal) {
-        var proactiveSlide = slide[slideCurrent + 1];
-    } else {
-        var proactiveSlide = slide[0];
-    }
-    var activeSlide = slide[slideCurrent];
-    if (slideCurrent > 0) {
-        var preactiveSlide = slide[slideCurrent - 1];
-    } else {
-        var preactiveSlide = slide[slideTotal];
-    }
+    const proactiveSlide = (slideCurrent < slideTotal) ? slide[slideCurrent + 1] : slide[0];
+    const activeSlide = slide[slideCurrent];
+    const preactiveSlide = (slideCurrent > 0) ? slide[slideCurrent - 1] : slide[slideTotal];
+
     slide.forEach((elem) => {
-        var thisSlide = elem;
-        if (thisSlide.classList.contains('proactive')) {
-            thisSlide.classList.remove('preactivede');
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.add('proactivede');
-        }
-        if (thisSlide.classList.contains('proactivede')) {
-            thisSlide.classList.remove('preactive');
-            thisSlide.classList.remove('active');
-            thisSlide.classList.remove('proactive');
-            thisSlide.classList.remove('proactivede');
-            thisSlide.classList.add('preactivede');
-        }
+        elem.classList.remove('proactive', 'preactive', 'active', 'proactivede');
+        elem.classList.add('proactivede');
     });
 
-    preactiveSlide.classList.remove('preactivede');
-    preactiveSlide.classList.remove('active');
-    preactiveSlide.classList.remove('proactive');
-    preactiveSlide.classList.remove('proactivede');
     preactiveSlide.classList.add('preactive');
-
-    activeSlide.classList.remove('preactivede');
-    activeSlide.classList.remove('preactive');
-    activeSlide.classList.remove('proactive');
-    activeSlide.classList.remove('proactivede');
     activeSlide.classList.add('active');
-
-    proactiveSlide.classList.remove('preactivede');
-    proactiveSlide.classList.remove('preactive');
-    proactiveSlide.classList.remove('active');
-    proactiveSlide.classList.remove('proactivede');
     proactiveSlide.classList.add('proactive');
 
     updateBullet();
 }
 
-
 function goToIndexSlide(index) {
-    const sliding = (slideCurrent > index) ? () => slideRight() : () => slideLeft();
     while (slideCurrent !== index) {
-        sliding();
+        if (slideCurrent < index) {
+            slideRight();
+        } else {
+            slideLeft();
+        }
     }
 }
 
-
 slideInitial();
-
-
-
-
